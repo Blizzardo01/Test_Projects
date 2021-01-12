b037ee102817dad61a1887e5d2cf3141d9821446
Blizzy <70334430+Blizzardo01@users.noreply.github.com>
2021-01-12 06:21:38 -0600

    Add files via upload

---
+++ source/repos/GradeBook/GradeBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+            book.AddGrade(90.1);
+
+            var grades = new List<double>() { 12, 10, 5, 8 };
+            grades.Add(56.0);
+            
+            double result = 0.0;
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+            result = result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
+         
+        }
+    }
+}
