95b0dbc2baed8a4a72cd044505247c0135e21312
Jacob Harris <jacobwho07@gmail.com>
2021-08-29 20:44:52 -0500

    finished the log for the temperature raw_input

--- BSAProgramming/BSA_py.py
+++ BSAProgramming/BSA_py.py
@@ -1,18 +1,24 @@
- 
+import datetime
 #...initialize looping variable, assume 'yes' as the first answer
+date = datetime.datetime.now()
 continueYN = "yes"
  
 while continueYN == "yes":
    #...get temperature input from the user
    sDegreeF = input("Enter next temperature in degrees Fahrenheit (F):")
- 
+
+
+   #writing the inputed temperature to the log-- "log.txt"
+   log = open("log.txt", "a")
+   log.write("Temperature entered: {}F|time: {}\n".format(sDegreeF, date.strftime("%c")))
+   log.close()
    #...convert text entry to number value that can be used in equations
    nDegreeF = int(sDegreeF)
  
    #...convert temperature from F to Celsius
    nDegreeC = (nDegreeF - 32) * 5 / 9
  
-   print (f"Temperature in degrees C is:{nDegreeC}")
+   print ("Temperature in degrees C is:{}".format(nDegreeC))
  
    #...check for temperature below freezing..
    if nDegreeC < 0:
@@ -25,5 +31,6 @@ while continueYN == "yes":
    if nDegreeF > 80:
       print ("Remember to hydrate!")
  
-   continueYN = input("Input another?")
+   continueYN = raw_input("Input another?")
+   #print (continueYN4)
 #exit the program
