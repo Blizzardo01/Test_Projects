173d930f0a44e7aafd9ce892a04724828b6f90db
Jacob <jacobwho07@gmail.com>
2021-08-31 23:12:58 -0500

    Math Program that calculates test averages together

---
+++ PY_Projects/Averages_Project/avg.py
@@ -0,0 +1,57 @@
+import math
+import cmath
+Starter = input("This program averages your scores together for you!\n Formula - %(A1) + %(A2) = %(Sum of A)")
+Howmany = input("How many inputs will you be providing?")
+FirstI = input("Enter the first input")
+SecondI = input("Enter the second input")
+ThirdI = input("Enter the third input")
+FourthI = input("Enter the fourth input")
+FifthI = input("Enter the Fifth input")
+SixthI = input("Enter the Sixth input")
+Equals = input("what will that equal?")
+equation = 
+average = 
+
+
+if Howmany == 2:
+  FirstI = input("Enter the first input")
+  SecondI = input("Enter the second input")
+  Equals = input("what will that equal?")
+  equation = FirstI + SecondI = Equals
+
+
+if Howmany == 3:
+  FirstI = input("Enter the first input")
+  SecondI = input("Enter the second input")
+  ThirdI = input("Enter the third input")
+  Equals = input("what will that equal?")
+  equation = FirstI + SecondI + ThirdI = Equals
+
+if Howmany == 4:
+  FirstI = input("Enter the first input")
+  SecondI = input("Enter the second input")
+  ThirdI = input("Enter the third input")
+  FourthI = input("Enter the fourth input")
+  Equals = input("what will that equal?")
+  equation = FirstI + SecondI + ThirdI + FourthI = Equals
+
+
+if Howmany == 5:
+  FirstI = input("Enter the first input")
+  SecondI = input("Enter the second input")
+  ThirdI = input("Enter the third input")
+  FourthI = input("Enter the fourth input")
+  FifthI = input("Enter the Fifth input")
+  Equals = input("what will that equal?")
+equation = FirstI + SecondI + ThirdI + FourthI + FifthI = Equals
+
+
+if Howmany == 6:
+  FirstI = input("Enter the first input")
+  SecondI = input("Enter the second input")
+  ThirdI = input("Enter the third input")
+  FourthI = input("Enter the fourth input")
+  FifthI = input("Enter the Fifth input")
+  SixthI = input("Enter the Sixth input")
+  Equals = input("what will that equal?")  
+  equation = FirstI + SecondI + ThirdI + FourthI + FifthI + SixthI = Equals
